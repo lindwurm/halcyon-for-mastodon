e2ca6dedce95b251abc7b500652c80c6868f8f02
新都心(Neet Shin) <nucx@dio-vox.com>
2017-06-02 20:48:30 +0530

    Add files via upload

---
+++ public_html/login/logout.php
@@ -0,0 +1,3 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="/assets/js/jquery-cookie/src/jquery.cookie.js"></script>
+<script>localStorage.clear();$.removeCookie('session');location.href="/login";</script>
