c9c43a255c1220843ab477c2483eba78c2988059
新都心(Neet Shin) <nucx@dio-vox.com>
2017-06-02 20:44:58 +0530

    Delete logout.php

--- public_html/login/logout.php
+++
@@ -1,3 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="/assets/js/jquery-cookie/src/jquery.cookie.js"></script>
-<script>localStorage.clear();$.removeCookie('session');location.href="/login";</script>
